4444c95b1eddb47de1f9df81067ee4406e6d6092
bagus arif arikusworo <bagusarifproject@gmail.com>
2023-12-11 14:31:59 +0700

    tambah deteksi nutrisi

---
+++ component/form.py
@@ -0,0 +1,95 @@
+from flask import Blueprint, jsonify, request
+import numpy as np
+import pandas as pd
+import json
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.pipeline import Pipeline
+
+
+form_routes = Blueprint('form_routes', __name__)
+
+url = './recipes.csv'
+dataset = pd.read_csv(url)
+max_Calories=2000
+max_daily_fat=100
+max_daily_Saturatedfat=13
+max_daily_Cholesterol=300
+max_daily_Sodium=2300
+max_daily_Carbohydrate=325
+max_daily_Fiber=40
+max_daily_Sugar=40
+max_daily_Protein=200
+max_list=[max_Calories,max_daily_fat,max_daily_Saturatedfat,max_daily_Cholesterol,max_daily_Sodium,max_daily_Carbohydrate,max_daily_Fiber,max_daily_Sugar,max_daily_Protein]
+
+
+@form_routes.route("/form", methods=["POST"])
+def form_prediction():
+    if request.method == "POST":
+        data=request.json
+        result = data.values()
+
+        # Convert object to a list
+        data = list(result)
+        
+        # Convert list to an array
+        numpyArray = np.array([data])
+        print(data)
+
+    def scaling(dataframe):
+        scaler=StandardScaler()
+        prep_data=scaler.fit_transform(dataframe.iloc[:,16:25].to_numpy())
+        return prep_data,scaler
+
+    def nn_predictor(prep_data):
+        neigh = NearestNeighbors(metric='cosine',algorithm='brute')
+        neigh.fit(prep_data)
+        return neigh
+
+    def build_pipeline(neigh,scaler,params):
+        transformer = FunctionTransformer(neigh.kneighbors,kw_args=params)
+        pipeline=Pipeline([('std_scaler',scaler),('NN',transformer)])
+        return pipeline
+
+    def extract_data(dataframe,ingredient_filter,max_nutritional_values):
+        extracted_data=dataframe.copy()
+        for column,maximum in zip(extracted_data.columns[16:25],max_nutritional_values):
+            extracted_data=extracted_data[extracted_data[column]<maximum]
+        if ingredient_filter!=None:
+            for ingredient in ingredient_filter:
+                extracted_data=extracted_data[extracted_data['RecipeIngredientParts'].str.contains(ingredient,regex=False)] 
+        return extracted_data
+
+    def apply_pipeline(pipeline,_input,extracted_data):
+        return extracted_data.iloc[pipeline.transform(_input)[0]]
+
+    def recommand(dataframe,_input,max_nutritional_values,ingredient_filter=None,params={'return_distance':False}):
+        extracted_data=extract_data(dataframe,ingredient_filter,max_nutritional_values)
+        prep_data,scaler=scaling(extracted_data)
+        neigh=nn_predictor(prep_data)
+        pipeline=build_pipeline(neigh,scaler,params)
+        return apply_pipeline(pipeline,_input,extracted_data)
+    
+    output=recommand(dataset,numpyArray,max_list)
+    id_column = 'RecipeId'
+    
+    # Convert DataFrame to JSON format with 'records' orientation
+    output_json = output.to_dict(orient='records')
+    
+    # Create a new dictionary with modified keys
+    modified_output = {str(entry[id_column]): entry for entry in output_json}
+    
+    # Save the modified JSON data to a file
+    with open('output.json', 'w') as file:
+        file.write(json.dumps(modified_output, indent=2))
+
+    return "Received and processed the data successfully!" 
+
+# Convert DataFrame to JSON format with 'records' orientation
+    # output=output.to_json()
+    # # Save the JSON data to a file
+    # with open('output', 'w') as file:
+    #     file.write(output)
+    # return "Received and processed the data successfully!"  # You can customize the response
---
+++ component/prediction.py
@@ -0,0 +1,103 @@
+from flask import Blueprint, jsonify, request, Flask
+import numpy as np
+import pandas as pd
+from werkzeug.utils import secure_filename
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import tensorflow as tf
+from tensorflow_addons.metrics import F1Score
+import pickle
+import os
+from PIL import Image
+import time
+import hashlib
+
+# Create Flask application
+app = Flask(__name__)
+
+# Configuration settings for the Flask app
+app.config["ALLOWED_EXTENSIONS"] = set(['png', 'jpg', 'jpeg'])
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['MODEL_FILE'] = 'mobilenetV2.h5'
+
+prediction_routes = Blueprint('prediction_routes', __name__)
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+# Hash agar setiap foto yang di up namanya beda
+def generate_unique_filename(original_filename):
+    timestamp = str(int(time.time()))
+    hash_object = hashlib.md5(original_filename.encode())
+    unique_hash = hash_object.hexdigest()[:8]
+    return f"{timestamp}_{unique_hash}_{original_filename}"
+
+# Load model with label
+model = load_model(app.config['MODEL_FILE'], compile=False)
+
+
+# Define the custom_objects dictionary
+custom_objects = {'F1Score': F1Score}
+
+# Load the model with custom_objects
+model = tf.keras.models.load_model('mobilenetV2.h5', custom_objects=custom_objects)
+
+# Now, you can use the 'model' object to make predictions or perform other operations.
+
+with open('class_indices_food_detection.pkl', 'rb') as indices:
+    loaded_indices = pickle.load(indices)
+
+@prediction_routes.route("/prediction", methods=["GET", "POST"])
+def prediction():
+    if request.method == "POST":
+        image = request.files["image"]
+        if image and allowed_file(image.filename):
+            filename = generate_unique_filename(secure_filename(image.filename))
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            def preds(image_path):
+                image = load_img(image_path, target_size=(224, 224))
+                image = img_to_array(image)
+                image = np.expand_dims(image, axis=0)
+                image = image / 255.0
+                predictions = model.predict(image)
+                predicted_class = np.argmax(predictions, axis=1)
+                class_labels = loaded_indices
+                return list(class_labels)[predicted_class[0]]
+
+            image_uploads = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            prediction_nutrition = preds(image_uploads)
+            preds = preds(image_uploads)
+            print(preds)
+
+            return jsonify({
+                "status": 200,
+                "message": "File uploaded and saved successfully",
+                "data": {
+                    "prediction": preds,
+                    "image_url": f"/{app.config['UPLOAD_FOLDER']}/{filename}"
+                }
+            }), 200
+        else:
+            return jsonify({
+                "status": {
+                    "code": 400,
+                    "message": "Client side error"
+                },
+                "data": None
+            }), 400
+    else:
+        return jsonify({
+            "status": {
+                "code": 405,
+                "message": "Method not allowed"
+            },
+            "data": None
+        }), 405
+
+# Register Blueprints
+app.register_blueprint(prediction_routes)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
