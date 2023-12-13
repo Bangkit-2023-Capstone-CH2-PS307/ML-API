613f51e8869d650c492842aed7f7f4afdcda9250
bagus arif arikusworo <bagusarifproject@gmail.com>
2023-12-13 23:15:56 +0700

    kurang model ml disesuaiin dengan namanya :D

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from component.form import form_routes
-# from component.prediction import prediction_routes
+from component.prediction import prediction_routes
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ app.config['MODEL_FILE'] = 'mobilenetV2.h5'
 
 # Register Blueprints
 app.register_blueprint(form_routes)
-# app.register_blueprint(prediction_routes)
+app.register_blueprint(prediction_routes)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
